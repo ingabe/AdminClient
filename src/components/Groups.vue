0ad7feed60e6340ab1ee2a9754d89b93feaba1be
Inga <inga.borodin@gmail.com>
2017-02-20 17:29:06 +0200

    Add grid to the Groups

--- src/components/Groups.vue
+++ src/components/Groups.vue
@@ -1,14 +1,49 @@
 <template>
-  <h1>{{message}}</h1>
+  <div>
+    <h1>{{message}}</h1>
+    <form id="search" class="searchForm">
+      <label>Search</label>
+      <input name="query" v-model="searchQuery">
+    </form>
+    <div class="groupsGrid">
+      <grid :data="gridData" :columns="gridColumns" :filter-key="searchQuery"></grid>
+    </div>
+  </div>
 </template>
 
 <script>
+import grid from './basicComponents/grid'
+import groupService from '../services/group.service'
+
 export default {
   name: 'Groups',
   data () {
     return {
-      message: 'Groups'
+      message: 'Groups',
+      searchQuery: '',
+      gridColumns: ['id', 'name', 'description'],
+      gridData: []
     }
+  },
+  created () {
+    groupService.getGroups().then(groups => this.gridData = groups)
+    return
+  },
+  components: {
+    grid
   }
 }
 </script>
+
+<style lang="scss" scoped>  
+
+    .searchForm{
+      margin: 50px;
+    }
+
+    .groupsGrid {
+      margin-top: 50px;
+      display: block;
+    }
+
+</style>
---
+++ src/services/group.service.js
@@ -0,0 +1,22 @@
+export function getGroups () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(generateGroups())
+    }, 1000)
+  })
+}
+
+function generateGroups () {
+  var groups = []
+
+  groups.push({id: 1, name: 'group 1', description: 'This is group 1'})
+  groups.push({id: 2, name: 'group 2', description: 'This is group 2'})
+  groups.push({id: 3, name: 'group 3', description: 'This is group 3'})
+  groups.push({id: 12, name: 'group 12', description: 'This is group 12'})
+
+  return groups
+}
+
+export default {
+  getGroups
+}
