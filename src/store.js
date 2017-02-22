5ba3991a7c3f0de82f5c81b3b2de83db667e5c44
Inga <inga.borodin@gmail.com>
2017-02-22 12:28:38 +0200

    Change the validation to the original

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import Vuex from 'vuex'
 import authModule from './modules/auth/auth.module'
 
-const isProduction = process.env.NODE_ENV === 'development' // 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default new Vuex.Store({
   modules: {
