e75c8a6d9a009f18709d12c3f52e9c9354e6b6cd
Inga <inga.borodin@gmail.com>
2017-02-22 12:12:33 +0200

    Edit style of ChangePassword and add a button

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -1,22 +1,24 @@
 <template> 
   <div>
     <h1>{{message}}</h1>
-    <form novalidate @submit.stop.prevent="submit">
+    <form novalidate @submit.stop.prevent="submit" class="changePasswordForm">
       
       <md-input-container>
         <label>User Name</label>
-        <md-input placeholder="User Name"></md-input>
+        <md-input placeholder="User Name" v-model="userName"></md-input>
       </md-input-container>
       
       <md-input-container>
         <label>Old Password</label>
-        <md-input placeholder="Old Password"></md-input>
+        <md-input placeholder="Old Password" v-model="oldPassword"></md-input>
       </md-input-container>
       
       <md-input-container>
         <label>New Password</label>
-        <md-input placeholder="New Password"></md-input>
+        <md-input placeholder="New Password" v-model="newPassword"></md-input>
       </md-input-container>
+
+      <md-button class="md-raised md-primary">Change Password</md-button>
 
     </form>
   </div>
@@ -33,8 +35,22 @@ export default {
       initialValue: 'My initial value'
     }
   },
+  props: {
+    userName: String,
+    oldPassword: String,
+    newPassword: String
+  },
   components: {
     VueMaterial
   }
 }
 </script>
+
+<style lang="scss" scoped>  
+
+    .changePasswordForm {
+         margin:auto;
+         width: 400px;
+    }
+
+</style>
